425f9865a5a28f17832b5897189da8d35ada3a71
yajima-yusuke <yajima-yusuke@dmm.com>
2019-07-23 17:07:29 +0900

    Fix slice

--- test/dto/user.go
+++ test/dto/user.go
@@ -5,18 +5,20 @@ import (
 )
 
 type User struct {
-	ID            string    `json:"id"`
-	Name          string    `json:"name"`
-	Age           int       `json:"age"`
-	Sex           Sex       `json:"sex"`
-	Weight        float64   `json:"weight"`
-	Alive         bool      `json:"alive"`
-	Num64         int64     `json:"num64"`
-	OptionalNum   *int32    `json:"optional_num"`
-	OptionalNum64 *int64    `json:"optional_num64"`
-	BirthDate     *string   `json:"birth_date"`
-	CreatedAt     time.Time `json:"created_at"`
-	ModifiedAt    time.Time `json:"modified_at"`
+	ID            string      `json:"id"`
+	Name          string      `json:"name"`
+	Age           int         `json:"age"`
+	Sex           Sex         `json:"sex"`
+	Weight        float64     `json:"weight"`
+	Alive         bool        `json:"alive"`
+	Num64         int64       `json:"num64"`
+	OptionalNum   *int32      `json:"optional_num"`
+	OptionalNum64 *int64      `json:"optional_num64"`
+	Numbers       []int64     `json:"numbers"`
+	Times         []time.Time `json:"times"`
+	BirthDate     *string     `json:"birth_date"`
+	CreatedAt     time.Time   `json:"created_at"`
+	ModifiedAt    time.Time   `json:"modified_at"`
 }
 
 type Sex string
